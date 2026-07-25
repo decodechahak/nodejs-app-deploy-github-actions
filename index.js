e949b8dd2e798da2f22e275a6b748c78c0c58505
decodechahak <agarwal07chahak@gmail.com>
2026-07-25 14:32:52 +0530

    Fix YAML syntax and formatting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res)=>{
-    return res.json({ msg: 'Hello from the server V1'})
+    return res.json({ msg: 'Hello from the server V1....CI/CD Pipeline is working'})
 })
 
 app.listen(PORT, () => {
